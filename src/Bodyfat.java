f45ba78ea84f2b11a51ae6157b9d46d4f6e9da30
Daniel Satnic <S01031109@acad.tri-c.edu>
2022-05-07 10:59:18 -0400

    oops have to use array

--- src/Bodyfat.java
+++ src/Bodyfat.java
@@ -1,16 +1,7 @@
 
 public class Bodyfat {
+	private BodyfatCell[] bodyfatCells;
 	
-	private int bodyfatAmount;
-
-	public int getBodyfatAmount() {
-		return bodyfatAmount;
-	}
-
-	public void setBodyfatAmount(int bodyfatAmount) {
-		this.bodyfatAmount = bodyfatAmount;
-	}
-
 	public void Test() {
 		System.out.println("Hello, world of bodyfat!");
 	}
