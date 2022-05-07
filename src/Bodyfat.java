13e177ff0786fb5d91969c679c8ca1906144aa7e
IT2030DSatnic <S01031109@acad.tri-c.edu>
2022-05-07 10:20:45 -0400

    set up bodyfat skeleton classes

---
+++ src/Bodyfat.java
@@ -0,0 +1,17 @@
+
+public class Bodyfat {
+	
+	private int bodyfatAmount;
+
+	public int getBodyfatAmount() {
+		return bodyfatAmount;
+	}
+
+	public void setBodyfatAmount(int bodyfatAmount) {
+		this.bodyfatAmount = bodyfatAmount;
+	}
+
+	public void Test() {
+		System.out.println("Hello, world of bodyfat!");
+	}
+}
---
+++ src/BodyfatSimulator.java
@@ -0,0 +1,8 @@
+
+public class BodyfatSimulator {
+	public static void main(String[] args) {
+		var bodyfat = new Bodyfat();
+		
+		bodyfat.Test();		
+	}
+}
