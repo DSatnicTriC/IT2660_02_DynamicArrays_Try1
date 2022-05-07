5c5cb4cb9b252b8fec7368cb0aadf7ace81b5655
Daniel Satnic <S01031109@acad.tri-c.edu>
2022-05-07 11:14:12 -0400

    set up the cell

--- src/BodyfatCell.java
+++ src/BodyfatCell.java
@@ -1,3 +1,12 @@
 
 public class BodyfatCell {
+	private int bodyfatAmount;
+	
+	public BodyfatCell(int initializeAmount) {
+		bodyfatAmount = initializeAmount;
+	}
+
+	public int getBodyfatAmount() {
+		return bodyfatAmount;
+	}
 }
